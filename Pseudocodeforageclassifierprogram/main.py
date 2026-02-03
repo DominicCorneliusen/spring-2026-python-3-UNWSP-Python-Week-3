bcefebe4f0e7b077b8638c1422c4a369954ff923
DominicCorneliusen <dmcorn.004@icloud.com>
2026-02-03 11:33:48 -0600

    Add files via upload

---
+++ Pseudocodeforageclassifierprogram/main.py
@@ -0,0 +1,15 @@
+#Author: Dominic Corneliusen
+#
+'''
+GET user age as integer
+
+IF user age <= 1
+    OUTPUT infant
+ELSEIF user age > 1 AND user age < 13
+    OUTPUT child
+ELSEIF user age >= 13 AND user age < 20
+    OUTPUT teenager
+ELSE user age >= 20
+    OUTPUT adult
+
+'''
